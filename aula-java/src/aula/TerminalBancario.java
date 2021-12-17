f26503500a0da687881bfb508d9c712adda41659
matlencar <malencar292@gmail.com>
2021-12-17 17:57:00 -0300

    exemplo primeiro comando em java 16/12/21

---
+++ aula-java/src/aula/TerminalBancario.java
@@ -0,0 +1,17 @@
+package aula;
+
+public class TerminalBancario {
+	public static void main(String[] args) {
+		
+		 int resultado = somar(2,3);
+		 
+		 System.out.println(resultado);
+
+	}
+	
+	public static int somar(int n1, int n2) {
+		int resultado = n1+n2;
+		
+		return resultado;
+	}
+}
